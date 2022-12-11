180d75875bd389e0516bbd449a9ccf06a34a2c47
Jiri Glinz <jglinz@gmail.com>
2022-12-11 11:50:13 +0100

    main js fix

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import lunch from './store/modules/lunch';
+import router from './router/index';
 
-import App from './App.vue'
-import router from './router'
+Vue.config.productionTip = false;
 
-import './assets/main.css'
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+const store = new Vuex.Store({
+  modules: {
+    lunch,
+  }
+});
 
 new Vue({
+  store,
   router,
-  render: (h) => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
